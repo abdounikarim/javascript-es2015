b641e780d1a76586a0292617785c57a80470cc9e
Karim <abdounikarim@gmail.com>
2019-05-11 16:08:05 +0200

    Template Strings

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -166,10 +166,7 @@
             //let {id, itemLabel, reps, totallyMadeUpKey = 'whatever'} = repLog;
             //console.log(id, itemLabel, reps, totallyMadeUpKey);
 
-            const tplText = $('#js-rep-log-row-template').html();
-            const tpl = _.template(tplText);
-
-            const html = tpl(repLog);
+            const html = rowTemplate(repLog);
             this.$wrapper.find('tbody').append($.parseHTML(html));
 
             this.updateTotalWeightLifted();
@@ -209,6 +206,21 @@
             return totalWeight;
         }
     }
+
+    const rowTemplate = (repLog) => `
+        <tr data-weight="${repLog.totalWeightLifted}">
+            <td>${repLog.itemLabel}</td>
+            <td>${repLog.reps}</td>
+            <td>${repLog.totalWeightLifted}</td>
+            <td>
+                <a href="#"
+                   class="js-delete-rep-log"
+                   data-url="${repLog.links._self}"
+                >
+                    <span class="fa fa-trash"></span>
+                </a>
+            </td>
+        </tr>`;
 
     window.RepLogApp = RepLogApp;
 
