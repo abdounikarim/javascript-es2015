9dd67d60166baf499b5a78db225b298a2acf71f4
Karim <abdounikarim@gmail.com>
2019-05-11 16:14:19 +0200

    The for of Loop

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -38,9 +38,9 @@
             $.ajax({
                 url: Routing.generate('rep_log_list'),
             }).then(data => {
-                $.each(data.items, (key, repLog) => {
+                for(let repLog of data.items) {
                     this._addRow(repLog);
-                });
+                }
             })
         }
 
@@ -96,9 +96,9 @@
 
             const $form = $(e.currentTarget);
             const formData = {};
-            $.each($form.serializeArray(), (key, fieldData) => {
+            for(let fieldData of $form.serializeArray()) {
                 formData[fieldData.name] = fieldData.value
-            });
+            };
             this._saveRepLog(formData)
             .then((data) => {
                 this._clearForm();
@@ -134,7 +134,7 @@
             this._removeFormErrors();
             const $form = this.$wrapper.find(RepLogApp._selectors.newRepForm);
 
-            $form.find(':input').each((index, element) => {
+            for(let element of $form.find(':input')) {
                 const fieldName = $(element).attr('name');
                 const $wrapper = $(element).closest('.form-group');
                 if (!errorData[fieldName]) {
@@ -146,7 +146,7 @@
                 $error.html(errorData[fieldName]);
                 $wrapper.append($error);
                 $wrapper.addClass('has-error');
-            });
+            };
         }
 
         _removeFormErrors() {
@@ -199,9 +199,9 @@
 
         static _calculateWeight($elements) {
             let totalWeight = 0;
-            $elements.each((index, element) => {
+            for (let element of $elements) {
                 totalWeight += $(element).data('weight');
-            });
+            };
 
             return totalWeight;
         }
